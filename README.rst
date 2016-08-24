6d105a5c6ed69e5930e78e3c7c7dec7bcd52f0d4
Loy van Beek <l.vanbeek@ultimaker.com>
2016-08-24 11:12:36 +0200

    Convert into Python package with setup and a README

---
+++ README.rst
@@ -0,0 +1,14 @@
+Python module for generating and parsing M2M format certificates
+================================================================
+
+.. _Python: https://www.python.org
+.. _GitHub: https://github.com/LoyVanBeek/m2m_certificates
+.. _issues: https://github.com/LoyVanBeek/m2m_certificates/issues
+.. _bugs: https://github.com/LoyVanBeek/m2m_certificates/labels/bug
+.. _questions: https://github.com/LoyVanBeek/m2m_certificates/labels/question
+.. _NFC Forum: http://nfc-forum.org/
+
+The **m2m_certificates** module implements the `NFC Forum`_ specification for M2M (Machine to Machine) certificates.
+This is a part of the NDEF specification.
+
+These certificates are more compact than standard x509 certificates and are thus easier to fit onto a NFC tag.
---
+++ setup.py
@@ -0,0 +1,38 @@
+"""Setup module for m2m_certificates.
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'm2m_certificates',
+    version = 0.1,
+    packages = find_packages(),
+    license = 'AGPL',
+    url = 'https://github.com/LoyVanBeek/m2m_certificates',
+
+    description = 'Python module for generating and parsing M2M format certificates',
+    long_description = long_description,
+    keywords = 'nfc ndef m2m',
+
+    author = 'Loy van Beek',
+    author_email = 'l.vanbeek@ultimaker.com',
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    install_requires = ['asn1crypto'],
+)
